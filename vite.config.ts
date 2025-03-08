f3648da4aa51fb2c0c847e7bfe383ebc55cd42b6
Anouar Aissaoui <tobia@crossbone.cc>
2025-03-08 03:17:03 +0100

    style: reformat

--- vite.config.ts
+++ vite.config.ts
@@ -5,28 +5,32 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA as pwa } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), pwa({
-    registerType: "autoUpdate",
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+  plugins: [
+    react(),
+    tailwindcss(),
+    pwa({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
-        name: 'Bébou',
-        short_name: 'Bébou',
-        description: 'Breathing exercise app to help during panic attacks',
-        theme_color: '#dab2ff',
+        name: "Bébou",
+        short_name: "Bébou",
+        description: "Breathing exercise app to help during panic attacks",
+        theme_color: "#dab2ff",
         icons: [
           {
-            src: 'pwa-192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'pwa-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-  })],
+            src: "pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
