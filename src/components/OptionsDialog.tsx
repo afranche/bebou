e87fd728fcb25ecb3dbac8053dd225d3f4e87ec6
Anouar Aissaoui <tobia@crossbone.cc>
2025-03-17 23:11:30 +0100

    feat(options): create a specific options dialog component

---
+++ src/components/OptionsDialog.tsx
@@ -0,0 +1,107 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Slider } from "@/components/ui/slider";
+import React from "react";
+
+const OptionsDialog: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <Dialog>
+      <DialogTrigger disabled>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Options</DialogTitle>
+          <DialogDescription>
+            <ul className="flex flex-col gap-5 mt-6">
+              <li className="flex">
+                <Slider
+                  id="slider"
+                  className="w-32"
+                  defaultValue={[5]}
+                  min={3}
+                  max={6}
+                  step={1}
+                />
+                <div className="grid gap-1.5 leading-none text-right grow">
+                  <label
+                    htmlFor="slider"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Respiration
+                  </label>
+                  <p className="text-xs text-muted-foreground">5 secondes</p>
+                </div>
+              </li>
+              <li className="flex">
+                <Checkbox id="timer" />
+                <div className="grid gap-1.5 leading-none text-right grow">
+                  <label
+                    htmlFor="timer"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Timer
+                  </label>
+                </div>
+              </li>
+              <li className="flex">
+                <Slider
+                  id="timer-slider"
+                  className="w-32"
+                  defaultValue={[5]}
+                  min={3}
+                  max={6}
+                  step={1}
+                />
+                <div className="grid gap-1.5 leading-none text-right grow">
+                  <label
+                    htmlFor="timer-slider"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Arrêter après
+                  </label>
+                  <p className="text-xs text-muted-foreground">6 minutes</p>
+                </div>
+              </li>
+              <li className="flex">
+                <Checkbox id="audio" />
+                <div className="grid gap-1.5 leading-none text-right grow">
+                  <label
+                    htmlFor="audio"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    Fond sonore (WIP)
+                  </label>
+                </div>
+              </li>
+              <li className="grid grid-cols-3 gap-2">
+                <Button
+                  className="text-purple-950 bg-purple-50 border border-purple-950"
+                  size="lg"
+                >
+                  Blanc
+                </Button>
+                <Button className="text-purple-950 bg-pink-400" size="lg">
+                  Rose
+                </Button>
+                <Button className="text-purple-50 bg-yellow-950" size="lg">
+                  Brun
+                </Button>
+              </li>
+            </ul>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default OptionsDialog;
--- src/hooks/timer.tsx
+++ src/hooks/timer.tsx
@@ -2,9 +2,25 @@ import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import type {} from "@redux-devtools/extension"; // required for devtools typing
 
+interface TimerConfigState {
+  period: number;
+  isUsingShorterPeriods: boolean;
+  globalTimerTarget: number;
+  isGlobalTimerEnabled: boolean;
+  isBackgroundNoiseEnabled: boolean;
+  noise: "white" | "pink" | "brown";
+  setPeriod: (newPeriod: number) => void;
+  setGlobalTimerTarget: (newPeriod: number) => void;
+  toggleShorterPeriods: () => void;
+  toggleGlobalTimer: () => void;
+  toggleBackgroundNoise: () => void;
+  selectBackgroundNoise: (newNoise: "white" | "pink" | "brown") => void;
+}
+
 interface TimerState {
   isRunning: boolean;
   timer: number;
+  globalTimer: number;
   countdown: number;
   phase: number;
   start: () => void;
@@ -13,21 +29,83 @@ interface TimerState {
   lap: (newTimer: number) => void;
 }
 
+// TODO: Turn into a slice to integrate into the timer store
+export const useTimerConfigStore = create<TimerConfigState>()(
+  devtools(
+    persist(
+      (set) => ({
+        period: 5,
+        isUsingShorterPeriods: true,
+        globalTimerTarget: 5,
+        isGlobalTimerEnabled: true,
+        isBackgroundNoiseEnabled: false,
+        noise: "white",
+
+        setPeriod: (newPeriod: number) =>
+          set(() => ({
+            period: newPeriod,
+          })),
+
+        setGlobalTimerTarget: (newGlobalTimerTarget: number) =>
+          set(() => ({
+            globalTimerTarget: newGlobalTimerTarget,
+          })),
+
+        toggleShorterPeriods: () =>
+          set((state) => ({
+            isUsingShorterPeriods: !state.isUsingShorterPeriods,
+          })),
+
+        toggleGlobalTimer: () =>
+          set((state) => ({
+            isGlobalTimerEnabled: !state.isGlobalTimerEnabled,
+          })),
+
+        toggleBackgroundNoise: () =>
+          set((state) => ({
+            isBackgroundNoiseEnabled: !state.isBackgroundNoiseEnabled,
+          })),
+
+        selectBackgroundNoise: (newNoise) =>
+          set(() => {
+            if (newNoise in ["white", "pink", "brown"])
+              return { noise: newNoise };
+            return {};
+          }),
+      }),
+      {
+        name: "timer-config-storage",
+      },
+    ),
+  ),
+);
+
 export const useTimerStore = create<TimerState>()(
   devtools(
     persist(
       (set) => ({
         isRunning: false,
         timer: 0,
+        globalTimer: 0,
         countdown: 0,
         phase: -1,
 
         start: () => set(() => ({ isRunning: true })),
 
         stop: () =>
-          set(() => ({ isRunning: false, phase: -1, timer: 0, countdown: 0 })),
+          set(() => ({
+            isRunning: false,
+            phase: -1,
+            timer: 0,
+            globalTimer: 0,
+            countdown: 0,
+          })),
 
-        tick: () => set((state) => ({ countdown: state.countdown - 1 })),
+        tick: () =>
+          set((state) => ({
+            countdown: state.countdown - 1,
+            globalTimer: state.globalTimer + 1,
+          })),
 
         lap: (newTimer) =>
           set((state) => ({
@@ -38,7 +116,7 @@ export const useTimerStore = create<TimerState>()(
       }),
       {
         name: "timer-storage",
-      }
-    )
-  )
+      },
+    ),
+  ),
 );
