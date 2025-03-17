ad5f1877506ccac77adea3c5f9904b5c7a6636d5
Anouar Aissaoui <tobia@crossbone.cc>
2025-03-17 23:12:23 +0100

    feat: use the options dialog component created prior

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,4 @@
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Slider } from "./components/ui/slider";
 
 import "./App.css";
 
@@ -16,6 +6,7 @@ import { useTimerStore } from "./hooks/timer";
 import BreathingGuideController from "./components/BreathingGuideController";
 
 import { Play, Bolt } from "lucide-react";
+import OptionsDialog from "./components/OptionsDialog";
 
 function App() {
   const { phase, isRunning, start, stop } = useTimerStore();
@@ -51,107 +42,12 @@ function App() {
                 <Play />
                 <span>Démarrer</span>
               </Button>
-              <Dialog>
-                <DialogTrigger disabled>
-                  <Button size="lg" variant="secondary" disabled>
-                    <Bolt />
-                    <span>Options (WIP)</span>
-                  </Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Options</DialogTitle>
-                    <DialogDescription>
-                      <ul className="flex flex-col gap-5 mt-6">
-                        <li className="flex">
-                          <Slider
-                            id="slider"
-                            className="w-32"
-                            defaultValue={[5]}
-                            min={3}
-                            max={6}
-                            step={1}
-                          />
-                          <div className="grid gap-1.5 leading-none text-right grow">
-                            <label
-                              htmlFor="slider"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Respiration
-                            </label>
-                            <p className="text-xs text-muted-foreground">
-                              5 secondes
-                            </p>
-                          </div>
-                        </li>
-                        <li className="flex">
-                          <Checkbox id="timer" />
-                          <div className="grid gap-1.5 leading-none text-right grow">
-                            <label
-                              htmlFor="timer"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Timer
-                            </label>
-                          </div>
-                        </li>
-                        <li className="flex">
-                          <Slider
-                            id="timer-slider"
-                            className="w-32"
-                            defaultValue={[5]}
-                            min={3}
-                            max={6}
-                            step={1}
-                          />
-                          <div className="grid gap-1.5 leading-none text-right grow">
-                            <label
-                              htmlFor="timer-slider"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Arrêter après
-                            </label>
-                            <p className="text-xs text-muted-foreground">
-                              6 minutes
-                            </p>
-                          </div>
-                        </li>
-                        <li className="flex">
-                          <Checkbox id="audio" />
-                          <div className="grid gap-1.5 leading-none text-right grow">
-                            <label
-                              htmlFor="audio"
-                              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                            >
-                              Fond sonore (WIP)
-                            </label>
-                          </div>
-                        </li>
-                        <li className="grid grid-cols-3 gap-2">
-                          <Button
-                            className="text-purple-950 bg-purple-50 border border-purple-950"
-                            size="lg"
-                          >
-                            Blanc
-                          </Button>
-                          <Button
-                            className="text-purple-950 bg-pink-400"
-                            size="lg"
-                          >
-                            Rose
-                          </Button>
-                          <Button
-                            className="text-purple-50 bg-yellow-950"
-                            size="lg"
-                          >
-                            Brun
-                          </Button>
-                        </li>
-                      </ul>
-                    </DialogDescription>
-                  </DialogHeader>
-                </DialogContent>
-              </Dialog>
+              <OptionsDialog>
+                <Button size="lg" variant="secondary" disabled>
+                  <Bolt />
+                  <span>Options (WIP)</span>
+                </Button>
+              </OptionsDialog>
             </div>
           </div>
         )}
